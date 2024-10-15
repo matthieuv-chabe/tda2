17df7ec0d78970f1919d480fa8f730cfb849c450
Your Name <you@example.com>
2024-10-15 18:31:27 +0200

    bonjomur

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { PublicClientApplication } from "@azure/msal-browser";
+
+import { msalConfig } from "./authConfig";
+import { MsalProvider } from "@azure/msal-react";
+import AppAuth from "./mainAuth.tsx";
+const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.createRoot(document.body).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<MsalProvider instance={msalInstance}>
+			<AppAuth />
+		</MsalProvider>
+	</React.StrictMode>
+);
---
+++ src/mainAuth.tsx
@@ -0,0 +1,38 @@
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import App from "./App";
+import { Button } from "@mui/material";
+
+import { loginRequest } from "./authConfig";
+
+export const SignInButton = () => {
+    const { instance } = useMsal();
+  
+    const handleLogin = (loginType) => {
+      if (loginType === "popup") {
+        instance.loginPopup(loginRequest).catch((e) => {
+          console.log(e);
+        });
+      } else if (loginType === "redirect") {
+        instance.loginRedirect(loginRequest).catch((e) => {
+          console.log(e);
+        });
+      }
+    };
+    return (
+
+        <Button onClick={() => handleLogin("redirect")}>
+          Sign in using Redirect
+        </Button>
+    );
+  };
+
+export default function AppAuth() {
+
+    const isAuthenticated = useIsAuthenticated();
+
+    if(!isAuthenticated) {
+        return <SignInButton />;
+    }
+
+    return <App />;
+}
