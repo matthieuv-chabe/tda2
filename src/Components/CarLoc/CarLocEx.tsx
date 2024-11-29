2f0c25e1d3f2d48a8eb19af43f7fe1777740e67c
Matthieu VANCAYZEELE <matthieu.vancayzeele@yzeel.fr>
2024-11-29 17:48:27 +0100

    u

--- src/Components/CarLoc/CarLocEx.tsx
+++ src/Components/CarLoc/CarLocEx.tsx
@@ -1,28 +1,7 @@
-import { useMap, useMapsLibrary, Marker } from "@vis.gl/react-google-maps";
-import { useEffect, useRef, useState } from "react";
+import { useMap, Marker } from "@vis.gl/react-google-maps";
+import { useEffect, useRef } from "react";
 import { LastKnownPositionInfo } from "../../App";
-import { GeolocExtrapolationComputer } from "../../core/utils/maps/maps";
-import { CarAlgorithms } from "../../CarAlgorithms";
-import { polyline_and_percent_to_latlng, polyline_and_percent_to_subpolyline } from "../../core/utils/maps/polyline";
 import { CarLocationManager } from "../../core/CarLocationManager";
-
-function calculateRotation(lat1, lon1, lat2, lon2) {
-    const toRadians = (degrees) => degrees * Math.PI / 180;
-    const toDegrees = (radians) => radians * 180 / Math.PI;
-    
-    const dLon = toRadians(lon2 - lon1);
-    
-    const y = Math.sin(dLon) * Math.cos(toRadians(lat2));
-    const x = Math.cos(toRadians(lat1)) * Math.sin(toRadians(lat2)) -
-              Math.sin(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.cos(dLon);
-    
-    let angle = toDegrees(Math.atan2(y, x));
-    return (angle + 360) % 360;  // to ensure the angle is between 0 and 360
-}
-
-// Example usage:
-const rotationAngle = calculateRotation(48.8566, 2.3522, 51.5074, -0.1278);
-console.log(rotationAngle);
 
 
 export const CarLocEx = (props: {
