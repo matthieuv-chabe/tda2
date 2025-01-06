8583f06bafe61a259d95a358e030df71d3e7aa88
Matthieu VANCAYZEELE <matthieu.vancayzeele@yzeel.fr>
2025-01-06 15:45:15 +0100

    remove usews

--- src/Hooks/useWs.ts
+++ src/Hooks/useWs.ts
@@ -1,21 +1,5 @@
 import { useEffect, useState } from "react";
 
 export const useWs = () => {
-    const [ws, setWs] = useState<WebSocket | null>(null);
     
-    useEffect(() => {
-        let ws = new WebSocket("ws://localhost:3001/ws");
-        ws.onerror = (e) => {
-            console.error(e);
-            ws = new WebSocket("ws://localhost:3001/ws");
-        }
-        
-        setWs(ws);
-    
-        return () => {
-            ws.close();
-        };
-    }, []);
-    
-    return ws;
     };
