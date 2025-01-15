c061878a8d7a51ba0e4e48cf4069e697d645927b
Matthieu VANCAYZEELE <matthieu.vancayzeele@yzeel.fr>
2025-01-15 13:50:43 +0100

    )p

--- src/business/missionWCategories.ts
+++ src/business/missionWCategories.ts
@@ -14,11 +14,20 @@ type ServiceTypesT = {
 const text = await (await fetch('/public/servicetypes.csv')).text()
 const c = new CsvCategorizer<ServiceTypesT>(text, {colSeparator:';', rowSeparator:'\r\n'})
 
+
+
 export const isWMissionTimeBased = c.factory(
 	// First filter the data to get the desired row
-	(obj, code, erp) => obj.ID === code && obj.Dispatch === erp,
+	(obj, code, erp) => {
+		const r = obj.ID === code && obj.Dispatch === erp
+		console.log({r})
+		return r
+	},
 	// Then transform the data to extract the information
-	obj => obj && obj.length == 1 && obj[0].Group2 == 'Time Based'
+	obj => {
+		console.log({objtodet: obj, tb: obj && obj.length == 1 && obj[0].Group2 == 'Time Based'})
+		return obj && obj.length == 1 && obj[0].Group2 == 'Time Based'
+	}
 )
 
 export const isWMissionMeetGreet = c.factory(
