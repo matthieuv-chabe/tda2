9805e06771d5a2393c12f5e23819b2f34b8806e3
Matthieu VANCAYZEELE <matthieu.vancayzeele@yzeel.fr>
2025-01-17 16:51:01 +0100

    upd

--- src/Components/OneMission.tsx
+++ src/Components/OneMission.tsx
@@ -91,16 +91,17 @@ export const OneMission = (props: {
 		}
 	}
 
-	// const is_mad = isWMissionTimeBased(props.mission.w.MIS_TSE_ID, CarLocationManager.first_dispatch);
-	if(true) {
-		const date = arrivalDefault.toLocaleTimeString().substring(0, 5);
+	const is_mad = isWMissionTimeBased(props.mission.w.MIS_TSE_ID, CarLocationManager.first_dispatch);
+	if(is_mad) {
 
-		if(date.indexOf("Inval") != -1) {
-			str = t('endOfMissionNotAvailable')
-		} else {
 			str = t('missionEndsAt') + " " + arrivalDefault.toLocaleTimeString().substring(0, 5);
-		}
 
+	}
+
+	const date = arrivalDefault.toLocaleTimeString().substring(0, 5);
+
+	if(date.indexOf("Inval") != -1) {
+		str = t('endOfMissionNotAvailable')
 	}
 
 	if(parseStatusFromRequest(props.mission.w) =='closed') return null;
